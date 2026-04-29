f34635d8eeeffb1aead0b918890418f149ca71c3
lucasrcruz7 <lucasrenatopro@gmail.com>
2026-04-29 09:43:37 -0300

    First Commit

---
+++ Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿namespace CrudNovoNN.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Cliente
+{
+    public int Id { get; set; }
+
+    [Required, StringLength(120)]
+    public string Nome { get; set; } = string.Empty;
+
+    [EmailAddress, StringLength(120)]
+    public string? Email { get; set; }
+
+    [StringLength(20)]
+    public string? Telefone { get; set; }
+
+    public ICollection<Venda>? Vendas { get; set; }
+}
+
